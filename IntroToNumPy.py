fab18217437cb49d450e5fdde3d6f47145b75cad
dvbckle <50149661+dvbckle@users.noreply.github.com>
2019-05-05 18:52:22 -0600

    Create IntroToNumPy.py

---
+++ IntroToNumPy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  5 18:45:19 2019
+
+@author: dvbckle
+"""
+#RoboGarden Intoduction to NumPy Mission
+
+import numpy as np
+
+x = np.array([1,2,3])
+print(sum(x))
